1f7e50522b867f47cc3eeb517e0015c2e9bb3f75
Farid Amimri <farid.amimri@gmail.com>
2022-09-08 19:47:31 +0100

    update folder structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,30 @@
-import Layout from "../components/layout/Layout"
-// import styles from "../styles/Home.module.css"
+/** @format */
 
-export default function Home() {
-  return <Layout>main</Layout>
+import Layout from '../components/layout/Layout'
+import styles from '../styles/Home.module.css'
+import SearchInput from '../components/searchInput/SearchInput'
+import CountriesTable from '../components/countriesTable/CountriesTable'
+
+// Get the data at the build time (will only be updated when we rebuild the project )
+export const getStaticProps = async () => {
+  const res = await fetch('https://restcountries.com/v3.1/all')
+  const countries = await res.json()
+
+  return {
+    props: {
+      countries
+    }
+  }
+}
+
+export default function Home({ countries }) {
+  return (
+    <Layout>
+      <div className={styles.counts}>Found {countries.length} countries</div>
+
+      <SearchInput />
+
+      <CountriesTable countries={countries} />
+    </Layout>
+  )
 }
