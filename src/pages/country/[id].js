69df1da467fddd0254a310f37061c9df7e2cfb03
Farid Amimri <farid.amimri@gmail.com>
2022-09-27 15:16:01 +0100

    Build responsive country page

--- src/pages/country/[id].js
+++ src/pages/country/[id].js
@@ -36,73 +36,76 @@ function Country({ country }) {
 
   return (
     <Layout title={country.name}>
-      <div className={styles.overview}>
-        <picture>
-          <img src={country.flag} alt={country.name}></img>
-          <h1 className={styles.overview_name}>{country.name}</h1>
-          <h4 className={styles.overview_region}>{country.region}</h4>
-        </picture>
+      <div className={styles.container}>
+        <div className={styles.overview}>
+          <picture>
+            <img src={country.flag} alt={country.name}></img>
+            <h1 className={styles.overview_name}>{country.name}</h1>
+            <h4 className={styles.overview_region}>{country.region}</h4>
+          </picture>
 
-        <div className={styles.overview_statistic}>
-          <div className={styles.overview_population}>
-            <div className={styles.overview_value}>{country.population}</div>
-            <div className={styles.overview_label}>Population</div>
-          </div>
+          <div className={styles.overview_statistic}>
+            <div className={styles.overview_population}>
+              <div className={styles.overview_value}>{country.population}</div>
+              <div className={styles.overview_label}>Population</div>
+            </div>
 
-          <div className={styles.overview_area}>
-            <div className={styles.overview_value}>{country.area}</div>
-            <div className={styles.overview_label}>Area</div>
-          </div>
-        </div>
-      </div>
-
-      <div className={styles.details}>
-        <h4 className={styles.details_heading}>Details</h4>
-
-        <div className={styles.details_content}>
-          <div className={styles.details_label}>Capital</div>
-          <div className={styles.details_value}>{country.capital}</div>
-        </div>
-
-        <div className={styles.details_content}>
-          <div className={styles.details_label}>Languages</div>
-          <div className={styles.details_value}>
-            {country.languages.map(({ name }) => name)}
-          </div>
-        </div>
-
-        <div className={styles.details_content}>
-          <div className={styles.details_label}>Currencies</div>
-          <div className={styles.details_value}>
-            {country.currencies.map(({ name }) => name)}
-          </div>
-        </div>
-
-        <div className={styles.details_content}>
-          <div className={styles.details_label}>Native name</div>
-          <div className={styles.details_value}>{country.nativeName}</div>
-        </div>
-
-        <div className={styles.details_content}>
-          <div className={styles.details_label}>Gini</div>
-          <div className={styles.details_value}>{country.gini} %</div>
-        </div>
-
-        <div className={styles.details_content}>
-          <div className={styles.details_label}>Subregion</div>
-          <div className={styles.details_value}>{country.subregion}</div>
-        </div>
-
-        <div className={styles.details_content}>
-          <div className={styles.details_borders}>
-            <div className={styles.details_label}>Neighbouring Countries</div>
-            <div className={styles.details_border}>
-              {borders.map((border, index) => (
-                <h4 key={index}>{border}</h4>
-              ))}
+            <div className={styles.overview_area}>
+              <div className={styles.overview_value}>{country.area}</div>
+              <div className={styles.overview_label}>Area</div>
             </div>
           </div>
         </div>
+
+        <div className={styles.details}>
+          <h4 className={styles.details_heading}>Details</h4>
+
+          <div className={styles.details_content}>
+            <div className={styles.details_label}>Capital</div>
+            <div className={styles.details_value}>{country.capital}</div>
+          </div>
+
+          <div className={styles.details_content}>
+            <div className={styles.details_label}>Languages</div>
+            <div className={styles.details_value}>
+              {country.languages.map(({ name }) => name)}
+            </div>
+          </div>
+
+          <div className={styles.details_content}>
+            <div className={styles.details_label}>Currencies</div>
+            <div className={styles.details_value}>
+              {country.currencies.map(({ name }) => name)}
+            </div>
+          </div>
+
+          <div className={styles.details_content}>
+            <div className={styles.details_label}>Native name</div>
+            <div className={styles.details_value}>{country.nativeName}</div>
+          </div>
+
+          <div className={styles.details_content}>
+            <div className={styles.details_label}>Gini</div>
+            <div className={styles.details_value}>{country.gini} %</div>
+          </div>
+
+          <div className={styles.details_content}>
+            <div className={styles.details_label}>Subregion</div>
+            <div className={styles.details_value}>{country.subregion}</div>
+          </div>
+
+          <div className={styles.details_content}>
+            <div className={styles.details_borders}>
+              <div className={styles.details_label}>Neighbouring Countries</div>
+              <div className={styles.details_border}>
+                {borders.map((border, index) => (
+                  <h4 key={index}>{border}</h4>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+
       </div>
     </Layout>
   )
