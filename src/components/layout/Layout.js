baf091ba2db8b8a8daefbe61a0628cb011c2f6a7
Farid Amimri <farid.amimri@gmail.com>
2022-10-05 16:16:39 +0100

    Add brightness theme

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,12 +1,37 @@
 /** @format */
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Head from 'next/head'
 import styles from './Layout.module.css'
 import Logo from './Logo.svg'
 import Image from 'next/image'
+import { FaAdjust } from 'react-icons/fa'
 
 function Layout({ children, title = 'World Ranks' }) {
+  const [theme, setTheme] = useState('light')
+
+  useEffect(() => {
+    document.documentElement.setAttribute(
+      'data-theme',
+      localStorage.getItem('theme')
+    )
+    setTheme(localStorage.getItem('theme'))
+  }, [])
+
+  function switchTheme() {
+    if (theme === 'light') {
+      saveTheme('dark')
+    } else {
+      saveTheme('light')
+    }
+  }
+
+  function saveTheme(theme) {
+    setTheme(theme)
+    localStorage.setItem('theme', theme)
+    document.documentElement.setAttribute('data-theme', theme)
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -16,6 +41,9 @@ function Layout({ children, title = 'World Ranks' }) {
 
       <header className={styles.header}>
         <Image src={Logo} alt='logo' />
+        <button className={styles.themeSwitcher} onClick={switchTheme}>
+          <FaAdjust />
+        </button>
       </header>
 
       <main className={styles.main}>{children}</main>
