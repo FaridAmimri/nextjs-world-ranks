ca31c765d2cd404737c97536dae5bcd75c6ac6da
Farid Amimri <farid.amimri@gmail.com>
2022-10-06 16:13:33 +0100

    Add Link in Header to go back to home page

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -5,7 +5,10 @@ import Head from 'next/head'
 import styles from './Layout.module.css'
 import Logo from './Logo.svg'
 import Image from 'next/image'
+import Link from 'next/link'
 import { FaAdjust } from 'react-icons/fa'
+import { FaGithub } from 'react-icons/fa'
+import { FaLinkedin } from 'react-icons/fa'
 
 function Layout({ children, title = 'World Ranks' }) {
   const [theme, setTheme] = useState('light')
@@ -40,7 +43,9 @@ function Layout({ children, title = 'World Ranks' }) {
       </Head>
 
       <header className={styles.header}>
-        <Image src={Logo} alt='logo' />
+        <Link href='/'>
+          <Image src={Logo} alt='logo' />
+        </Link>
         <button className={styles.themeSwitcher} onClick={switchTheme}>
           <FaAdjust />
         </button>
@@ -48,7 +53,21 @@ function Layout({ children, title = 'World Ranks' }) {
 
       <main className={styles.main}>{children}</main>
 
-      <footer className={styles.footer}>www.faridamimri.com</footer>
+      <footer className={styles.footer}>
+        <div className={styles.socialMedia}>
+          <a href='https://www.linkedin.com/in/faridamimri/' target='blank'>
+            <FaLinkedin />
+          </a>
+          <a href='https://github.com/FaridAmimri' target='blank'>
+            <FaGithub />
+          </a>
+        </div>
+        <div className={styles.portfolio}>
+          <a href='https://www.faridamimri.com/' target='blank'>
+            &copy; 2022 www.faridamimri.com
+          </a>
+        </div>
+      </footer>
     </div>
   )
 }
